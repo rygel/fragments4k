946d7a0f0970aceb8babb713d474870a31f93f79
Alexander Brandt <alexander.brandt@outerstellar.de>
2026-05-13 18:48:07 +0300

    style: fix ktlint formatting in ErrorResponse

--- fragments-adapter-core/src/main/kotlin/io/github/rygel/fragments/adapter/ErrorResponse.kt
+++ fragments-adapter-core/src/main/kotlin/io/github/rygel/fragments/adapter/ErrorResponse.kt
@@ -7,7 +7,9 @@ data class ErrorResponse(
 ) {
     companion object {
         fun badRequest(message: String) = ErrorResponse(400, "Bad Request", message)
+
         fun notFound(message: String) = ErrorResponse(404, "Not Found", message)
+
         fun internalError() = ErrorResponse(500, "Internal Server Error", "An unexpected error occurred")
     }
 }
